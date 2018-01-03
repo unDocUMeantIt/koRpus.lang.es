65a7329ed3e9801ffe1f0ae51a1cd67db14414c3
unDocUMeantIt <m@reaktanz.de>
2018-01-03 18:13:10 +0100

    initial commit

---
+++ R/koRpus.lang.es-package.R
@@ -0,0 +1,25 @@
+#' Language Support for 'koRpus' Package: Spanish.
+#'
+#' \tabular{ll}{
+#' Package: \tab koRpus.lang.es\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1-1\cr
+#' Date: \tab 2018-01-03\cr
+#' Depends: \tab R (>= 3.1),koRpus (>= 0.11-2)\cr
+#' Encoding: \tab UTF-8\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes\cr
+#' URL: \tab https://reaktanz.de/?c=hacking&s=koRpus\cr
+#' }
+#'
+#' Adds support for the Spanish language to the 'koRpus' package. To ask for help, report
+#' bugs, suggest feature improvements, or discuss the global development of the package, please
+#' consider subscribing to the koRpus-dev mailing list (<http://korpusml.reaktanz.de>).
+#'
+#' @aliases koRpus.lang.es-package
+#' @name koRpus.lang.es-package
+#' @docType package
+#' @title The koRpus.lang.es Package
+#' @author Meik Michalke, with contributions from Earl Brown
+#' @keywords package
+NULL
